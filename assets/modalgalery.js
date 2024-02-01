7768122749b11aad95eb481aeafdf5e605fd60e6
216120191125 <136864167+216120191125@users.noreply.github.com>
2024-02-01 22:42:32 +0100

    update(debugging-carousel)

---
+++ assets/modalgalery.js
@@ -0,0 +1,25 @@
+let images = ["./assets/images/gallery/concerts/gallery-image-0.png", "./assets/images/gallery/entreprise/gallery-image-1.jpg", "./assets/images/gallery/entreprise/gallery-image-2.jpg", "./assets/images/gallery/mariage/gallery-image-3.jpg", "./assets/images/gallery/portraits/gallery-image-4.jpg", "./assets/images/gallery/mariage/gallery-image-5.jpg", "./assets/images/gallery/portraits/gallery-image-6.jpg", "./assets/images/gallery/concerts/gallery-image-7.jpg", "./assets/images/gallery/entreprise/gallery-image-8.jpg"]
+
+const galery = Array.from(document.querySelectorAll(".gallery img"))
+galery.forEach(image => {
+    image.addEventListener("click", function(e) {
+        let modalId = e.target.id
+        const arrowPrev = document.querySelector(".mg-prev")
+        const arrowNext = document.querySelector(".mg-next")
+        const imageModal = document.querySelector(".lightboxImage")
+        if (modalId === 0) {
+            arrowPrev.classList.add("arrow-hidden-true")
+        }
+        arrowNext.addEventListener("click", function() {
+            modalId ++
+            imageModal.src = images[modalId]
+            console.log(modalId)
+        })
+        arrowPrev.addEventListener("click", function() {
+            modalId--
+            imageModal.src = images[modalId]
+            console.log(modalId)
+        })
+    })
+}); 
+
