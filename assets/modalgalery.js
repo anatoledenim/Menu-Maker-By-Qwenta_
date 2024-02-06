f67a366c489b4cb6fb25243a817b924f416ac0db
216120191125 <136864167+216120191125@users.noreply.github.com>
2024-02-06 19:19:24 +0100

    update(debug-jquery-dom)

--- assets/modalgalery.js
+++ assets/modalgalery.js
@@ -1,34 +1,39 @@
 let images = ["./assets/images/gallery/concerts/gallery-image-0.png", "./assets/images/gallery/entreprise/gallery-image-1.jpg", "./assets/images/gallery/entreprise/gallery-image-2.jpg", "./assets/images/gallery/mariage/gallery-image-3.jpg", "./assets/images/gallery/portraits/gallery-image-4.jpg", "./assets/images/gallery/mariage/gallery-image-5.jpg", "./assets/images/gallery/portraits/gallery-image-6.jpg", "./assets/images/gallery/concerts/gallery-image-7.jpg", "./assets/images/gallery/entreprise/gallery-image-8.jpg"]
 
+
+function displayArrow(modalId, arrowPrev, arrowNext) {
+    if (modalId === 0) {
+        arrowPrev.classList.add("arrow-hidden-true")
+        arrowNext.classList.remove("arrow-hidden-true")
+    } else if (modalId === 8) {
+        arrowPrev.classList.remove("arrow-hidden-true")
+        arrowNext.classList.add("arrow-hidden-true")
+    } else {
+        arrowPrev.classList.remove("arrow-hidden-true")
+        arrowNext.classList.remove("arrow-hidden-true")
+    }
+}
+
+
 const galery = Array.from(document.querySelectorAll(".gallery img"))
 galery.forEach(image => {
     image.addEventListener("click", function(e) {
-        let modalId = e.target.id
         const arrowPrev = document.querySelector(".mg-prev")
         const arrowNext = document.querySelector(".mg-next")
         const imageModal = document.querySelector(".lightboxImage")
-        if (modalId === 0) {
+        let modalId = e.target.dataset.id
+        if (modalId === 2) {
             arrowPrev.classList.add("arrow-hidden-true")
         }
         arrowNext.addEventListener("click", function() {
             modalId ++
             imageModal.src = images[modalId]
-            if (modalId !== 0) {
-                arrowPrev.classList.remove("arrow-hidden-true")
-            }
-            if (modalId === 8) {
-                arrowNext.classList.add("arrow-hidden-true")
-            }
+            displayArrow(modalId, arrowPrev, arrowNext)
         })
         arrowPrev.addEventListener("click", function() {
-            modalId--
+            modalId --
             imageModal.src = images[modalId]
-            if (modalId === 0 ) {
-                arrowPrev.classList.add("arrow-hidden-true")
-            }
-            if (modalId !== 8) {
-                arrowNext.classList.remove("arrow-hidden-true")
-            }
+            displayArrow(modalId, arrowPrev, arrowNext)
         })
     })
 }); 
